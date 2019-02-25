b7d545273b41cf1560b4c6f273592f36e27252e6
Samuel Georges <sam@daftspunk.com>
2019-02-25 17:51:29 +1100

    Spaces -> tabs

--- pkg/txbuilder/tx.go
+++ pkg/txbuilder/tx.go
@@ -1,23 +1,23 @@
 package txbuilder
 
 import (
-    "fmt"
+	"fmt"
 
-    "github.com/btcsuite/btcd/chaincfg/chainhash"
-    "github.com/btcsuite/btcutil"
-    "github.com/tokenized/smart-contract/pkg/wire"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcutil"
+	"github.com/tokenized/smart-contract/pkg/wire"
 )
 
 const (
-    OutputTypeP2PK TxOutputType = iota
-    OutputTypeReturn
+	OutputTypeP2PK TxOutputType = iota
+	OutputTypeReturn
 )
 
 type Tx struct {
-    Type       TxOutputType
-    SelfPkHash []byte
-    MsgTx      *wire.MsgTx
-    Inputs     []*TxInput
+	Type       TxOutputType
+	SelfPkHash []byte
+	MsgTx      *wire.MsgTx
+	Inputs     []*TxInput
 }
 
 type TxOutSortByValue []*TxOutput
@@ -25,44 +25,44 @@ type TxOutSortByValue []*TxOutput
 func (txOuts TxOutSortByValue) Len() int      { return len(txOuts) }
 func (txOuts TxOutSortByValue) Swap(i, j int) { txOuts[i], txOuts[j] = txOuts[j], txOuts[i] }
 func (txOuts TxOutSortByValue) Less(i, j int) bool {
-    return txOuts[i].Value > txOuts[j].Value
+	return txOuts[i].Value > txOuts[j].Value
 }
 
 type TxInput struct {
-    PkHash    []byte
-    PrevIndex int64
-    PrevHash  string
-    Value     uint64
+	PkHash    []byte
+	PrevIndex int64
+	PrevHash  string
+	Value     uint64
 }
 
 type TxOutput struct {
-    Type            TxOutputType
-    PkScript        []byte
-    PkHash          []byte
-    Address         btcutil.Address
-    Index           uint32
-    Value           uint64
-    TransactionHash []byte
-    Data            []byte
+	Type            TxOutputType
+	PkScript        []byte
+	PkHash          []byte
+	Address         btcutil.Address
+	Index           uint32
+	Value           uint64
+	TransactionHash []byte
+	Data            []byte
 }
 
 func (t TxOutput) GetHashString() string {
-    hash, err := chainhash.NewHash(t.TransactionHash)
-    if err != nil {
-        return ""
-    }
-    return fmt.Sprintf("out:%s:%d", hash.String(), t.Index)
+	hash, err := chainhash.NewHash(t.TransactionHash)
+	if err != nil {
+		return ""
+	}
+	return fmt.Sprintf("out:%s:%d", hash.String(), t.Index)
 }
 
 type TxOutputType uint
 
 func (s TxOutputType) String() string {
-    switch s {
-    case OutputTypeP2PK:
-        return StringP2pk
-    case OutputTypeReturn:
-        return StringReturn
-    default:
-        return "unknown"
-    }
+	switch s {
+	case OutputTypeP2PK:
+		return StringP2pk
+	case OutputTypeReturn:
+		return StringReturn
+	default:
+		return "unknown"
+	}
 }
