99170b9812f05ae4ce29a4d4fb557bdccbebfcc4
Curtis Ellis <curtis@nextcash.tech>
2019-05-12 23:35:54 -0600

    Remove tokenized signature filter. https://github.com/tokenized/specification/issues/47

--- cmd/smartcontractd/filters/tx.go
+++ cmd/smartcontractd/filters/tx.go
@@ -12,15 +12,6 @@ import (
 	"github.com/btcsuite/btcd/chaincfg"
 )
 
-var (
-	// Tokenized.com OP_RETURN script signature
-	// 0x6a <OP_RETURN>
-	// 0x0d <Push next 13 bytes>
-	// 0x746f6b656e697a65642e636f6d <"tokenized.com">
-	tokenizedSignature     = []byte{0x6a, 0x0d, 0x74, 0x6f, 0x6b, 0x65, 0x6e, 0x69, 0x7a, 0x65, 0x64, 0x2e, 0x63, 0x6f, 0x6d}
-	testTokenizedSignature = []byte{0x6a, 0x12, 0x74, 0x65, 0x73, 0x74, 0x2e, 0x74, 0x6f, 0x6b, 0x65, 0x6e, 0x69, 0x7a, 0x65, 0x64, 0x2e, 0x63, 0x6f, 0x6d}
-)
-
 // Filters for transactions with tokenized.com op return scripts.
 type TxFilter struct {
 	chainParams  *chaincfg.Params
@@ -45,20 +36,6 @@ func (filter *TxFilter) IsRelevant(ctx context.Context, tx *wire.MsgTx) bool {
 		logger.LogDepth(logger.ContextWithOutLogSubSystem(ctx), logger.LevelInfo, 3,
 			"Matches Tracer : %s", tx.TxHash().String())
 		return true
-	}
-
-	containsTokenized := false
-	for _, output := range tx.TxOut {
-		if IsTokenizedOpReturn(output.PkScript, filter.isTest) {
-			logger.LogDepth(logger.ContextWithOutLogSubSystem(ctx), logger.LevelInfo, 3,
-				"Matches TokenizedFilter : %s", tx.TxHash().String())
-			containsTokenized = true
-			break
-		}
-	}
-
-	if !containsTokenized {
-		return false
 	}
 
 	// Check if relevant to contract
@@ -94,19 +71,4 @@ func (filter *TxFilter) IsRelevant(ctx context.Context, tx *wire.MsgTx) bool {
 	}
 
 	return false
-}
-
-// Checks if a script carries the tokenized.com protocol signature
-func IsTokenizedOpReturn(pkScript []byte, isTest bool) bool {
-	if isTest {
-		if len(pkScript) < len(testTokenizedSignature) {
-			return false
-		}
-		return bytes.Equal(pkScript[:len(testTokenizedSignature)], testTokenizedSignature)
-	}
-
-	if len(pkScript) < len(tokenizedSignature) {
-		return false
-	}
-	return bytes.Equal(pkScript[:len(tokenizedSignature)], tokenizedSignature)
 }
