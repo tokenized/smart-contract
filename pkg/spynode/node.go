f9a9271beded9781cac7a3d942faedc23dea7941
Curtis Ellis <curtis@nextcash.tech>
2019-06-28 14:45:27 -0600

    Fix log message.

--- pkg/spynode/node.go
+++ pkg/spynode/node.go
@@ -771,7 +771,7 @@ func (node *Node) check(ctx context.Context) error {
 		}
 
 		if node.queueOutgoing(headerRequest) {
-			logger.Debug(ctx, "Requesting headers after : %s", headerRequest.getheaders.BlockLocatorHashes[0])
+			logger.Debug(ctx, "Requesting headers after : %s", headerRequest.BlockLocatorHashes[0])
 			node.state.MarkHeadersRequested()
 		}
 	}
