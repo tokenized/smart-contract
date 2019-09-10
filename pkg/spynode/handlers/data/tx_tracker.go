64d9ac5e91227a2bfc4a7993a6ea59f676893cf2
Curtis Ellis <curtis@nextcash.tech>
2019-09-10 13:33:12 -0600

    Re-enable spynode tx tracker.

--- pkg/spynode/handlers/data/tx_tracker.go
+++ pkg/spynode/handlers/data/tx_tracker.go
@@ -2,11 +2,14 @@ package data
 
 import (
 	"context"
-	sync "github.com/sasha-s/go-deadlock"
 	"time"
+	sync "github.com/sasha-s/go-deadlock"
 
 	"github.com/tokenized/smart-contract/pkg/bitcoin"
+	"github.com/tokenized/smart-contract/pkg/logger"
 	"github.com/tokenized/smart-contract/pkg/wire"
+
+	"github.com/pkg/errors"
 )
 
 // TxTracker saves txids that have been announced by a specific node, but not requested yet.
@@ -38,9 +41,9 @@ func (tracker *TxTracker) Add(txid *bitcoin.Hash32) {
 	tracker.mutex.Lock()
 	defer tracker.mutex.Unlock()
 
-	// if _, exists := tracker.txids[*txid]; !exists {
-	// 	tracker.txids[*txid] = time.Now()
-	// }
+	if _, exists := tracker.txids[*txid]; !exists {
+		tracker.txids[*txid] = time.Now()
+	}
 }
 
 // Call when a tx is received to cancel tracking
@@ -49,12 +52,12 @@ func (tracker *TxTracker) Remove(ctx context.Context, txids []*bitcoin.Hash32) {
 	defer tracker.mutex.Unlock()
 
 	// Iterate tracker ids first since that list should be much smaller
-	// for _, removeid := range txids {
-	// 	if time, exists := tracker.txids[*removeid]; exists {
-	// 		logger.Verbose(ctx, "Removing tracking for tx (%s) : %s", time.Format("15:04:05.999999"), removeid.String())
-	// 		delete(tracker.txids, *removeid)
-	// 	}
-	// }
+	for _, removeid := range txids {
+		if time, exists := tracker.txids[*removeid]; exists {
+			logger.Verbose(ctx, "Removing tracking for tx (%s) : %s", time.Format("15:04:05.999999"), removeid.String())
+			delete(tracker.txids, *removeid)
+		}
+	}
 }
 
 // Called periodically to request any txs that have not been received yet
@@ -62,40 +65,39 @@ func (tracker *TxTracker) Check(ctx context.Context, mempool *MemPool) ([]wire.M
 	tracker.mutex.Lock()
 	defer tracker.mutex.Unlock()
 
-	// response := []wire.Message{}
-	// invRequest := wire.NewMsgGetData()
-	// for txid, _ := range tracker.txids {
-	// 	newTxId := txid // Make a copy to ensure the value isn't overwritten by the next iteration
-	// 	alreadyHave, shouldRequest := mempool.AddRequest(&newTxId)
-	// 	if alreadyHave {
-	// 		delete(tracker.txids, txid) // Remove since we have received tx
-	// 	} else {
-	// 		if shouldRequest {
-	// 			logger.Verbose(ctx, "Re-Requesting tx : %s", newTxId.String())
-	// 			item := wire.NewInvVect(wire.InvTypeTx, &newTxId)
-	// 			// Request
-	// 			if err := invRequest.AddInvVect(item); err != nil {
-	// 				// Too many requests for one message
-	// 				response = append(response, invRequest) // Append full message
-	// 				invRequest = wire.NewMsgGetData()       // Start new message
-	//
-	// 				// Try to add it again
-	// 				if err := invRequest.AddInvVect(item); err != nil {
-	// 					return response, errors.Wrap(err, "Failed to add tx to get data request")
-	// 				} else {
-	// 					delete(tracker.txids, txid) // Remove since we requested
-	// 				}
-	// 			} else {
-	// 				delete(tracker.txids, txid) // Remove since we requested
-	// 			}
-	// 		} // else wait and check again later
-	// 	}
-	// }
-	//
-	// if len(invRequest.InvList) > 0 {
-	// 	response = append(response, invRequest)
-	// }
-	//
-	// return response, nil
-	return nil, nil
+	response := []wire.Message{}
+	invRequest := wire.NewMsgGetData()
+	for txid, _ := range tracker.txids {
+		newTxId := txid // Make a copy to ensure the value isn't overwritten by the next iteration
+		alreadyHave, shouldRequest := mempool.AddRequest(&newTxId)
+		if alreadyHave {
+			delete(tracker.txids, txid) // Remove since we have received tx
+		} else {
+			if shouldRequest {
+				logger.Verbose(ctx, "Re-Requesting tx : %s", newTxId.String())
+				item := wire.NewInvVect(wire.InvTypeTx, &newTxId)
+				// Request
+				if err := invRequest.AddInvVect(item); err != nil {
+					// Too many requests for one message
+					response = append(response, invRequest) // Append full message
+					invRequest = wire.NewMsgGetData()       // Start new message
+
+					// Try to add it again
+					if err := invRequest.AddInvVect(item); err != nil {
+						return response, errors.Wrap(err, "Failed to add tx to get data request")
+					} else {
+						delete(tracker.txids, txid) // Remove since we requested
+					}
+				} else {
+					delete(tracker.txids, txid) // Remove since we requested
+				}
+			} // else wait and check again later
+		}
+	}
+
+	if len(invRequest.InvList) > 0 {
+		response = append(response, invRequest)
+	}
+
+	return response, nil
 }
