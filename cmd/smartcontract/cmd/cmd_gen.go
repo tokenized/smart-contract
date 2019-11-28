bb220b09e90751f1d70dc661b1ef0a2abb5a2fd1
Curtis Ellis <curtis@nextcash.tech>
2019-11-27 22:19:24 -0700

    Add gen command option to generate xkeys.

--- cmd/smartcontract/cmd/cmd_gen.go
+++ cmd/smartcontract/cmd/cmd_gen.go
@@ -10,12 +10,43 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const (
+	FlagX  = "x"
+	FlagXs = "xs"
+)
+
 var cmdGen = &cobra.Command{
 	Use:   "gen",
 	Short: "Generates a bitcoin private key in WIF",
 	RunE: func(c *cobra.Command, args []string) error {
 		if len(args) != 0 {
 			return errors.New("Incorrect argument count")
+		}
+
+		extended, _ := c.Flags().GetBool(FlagX)
+		if extended {
+			key, err := bitcoin.GenerateMasterExtendedKey()
+			if err != nil {
+				fmt.Printf("Failed to generate extended key : %s\n", err)
+				return nil
+			}
+
+			fmt.Printf("XKey : %s\n", key.String())
+			return nil
+		}
+
+		extendedMulti, _ := c.Flags().GetBool(FlagXs)
+		if extendedMulti {
+			key, err := bitcoin.GenerateMasterExtendedKey()
+			if err != nil {
+				fmt.Printf("Failed to generate extended key : %s\n", err)
+				return nil
+			}
+
+			keys := bitcoin.ExtendedKeys{key}
+
+			fmt.Printf("XKeys : %s\n", keys.String())
+			return nil
 		}
 
 		network := network(c)
@@ -44,4 +75,6 @@ var cmdGen = &cobra.Command{
 }
 
 func init() {
+	cmdGen.Flags().Bool(FlagX, false, "generate an extended key")
+	cmdGen.Flags().Bool(FlagXs, false, "generate an multi-extended key")
 }
