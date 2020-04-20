64311de2beb9f71c2d919eec48853ec77c485f6b
Curtis Ellis <curtis@nextcash.tech>
2020-04-19 20:33:16 -0600

    Update identity oracle for minor endpoint changes.

--- pkg/identity/identity_test.go
+++ pkg/identity/identity_test.go
@@ -6,36 +6,93 @@ import (
 
 	"github.com/tokenized/smart-contract/pkg/bitcoin"
 	"github.com/tokenized/specification/dist/golang/actions"
+	"github.com/tokenized/specification/dist/golang/assets"
+	"github.com/tokenized/specification/dist/golang/protocol"
 )
 
 const baseURL = "http://localhost:8081"
 
-func TestRegister(t *testing.T) {
-	ctx := context.Background()
+// func TestRegister(t *testing.T) {
+// 	ctx := context.Background()
 
-	key, err := bitcoin.GenerateKey(bitcoin.MainNet)
-	if err != nil {
-		t.Fatalf("Failed to generate key : %s", err)
-	}
+// 	key, err := bitcoin.GenerateKey(bitcoin.MainNet)
+// 	if err != nil {
+// 		t.Fatalf("Failed to generate key : %s", err)
+// 	}
 
-	or, err := GetOracle(ctx, baseURL, key)
-	if err != nil {
-		t.Fatalf("Failed to get oracle : %s", err)
-	}
+// 	or, err := GetOracle(ctx, baseURL, key)
+// 	if err != nil {
+// 		t.Fatalf("Failed to get oracle : %s", err)
+// 	}
 
-	entity := actions.EntityField{
-		Name: "Test",
-	}
+// 	entity := actions.EntityField{
+// 		Name: "Test",
+// 	}
 
-	xkey, err := bitcoin.GenerateMasterExtendedKey()
-	if err != nil {
-		t.Fatalf("Failed to generate xkey : %s", err)
-	}
+// 	xkey, err := bitcoin.GenerateMasterExtendedKey()
+// 	if err != nil {
+// 		t.Fatalf("Failed to generate xkey : %s", err)
+// 	}
 
-	id, err := or.RegisterUser(ctx, entity, []bitcoin.ExtendedKeys{bitcoin.ExtendedKeys{xkey}})
-	if err != nil {
-		t.Fatalf("Failed to register user : %s", err)
-	}
+// 	id, err := or.RegisterUser(ctx, entity, []bitcoin.ExtendedKeys{bitcoin.ExtendedKeys{xkey}})
+// 	if err != nil {
+// 		t.Fatalf("Failed to register user : %s", err)
+// 	}
 
-	t.Logf("User ID : %s", id)
-}
+// 	t.Logf("User ID : %s", id)
+// }
+
+// func TestApproveReceive(t *testing.T) {
+// 	ctx := context.Background()
+
+// 	key, err := bitcoin.GenerateKey(bitcoin.MainNet)
+// 	if err != nil {
+// 		t.Fatalf("Failed to generate key : %s", err)
+// 	}
+
+// 	or, err := GetOracle(ctx, baseURL, key)
+// 	if err != nil {
+// 		t.Fatalf("Failed to get oracle : %s", err)
+// 	}
+
+// 	entity := actions.EntityField{
+// 		Name: "Test",
+// 	}
+
+// 	xkey, err := bitcoin.GenerateMasterExtendedKey()
+// 	if err != nil {
+// 		t.Fatalf("Failed to generate xkey : %s", err)
+// 	}
+
+// 	xpubs := bitcoin.ExtendedKeys{xkey}
+
+// 	userID, err := or.RegisterUser(ctx, entity, []bitcoin.ExtendedKeys{xpubs})
+// 	if err != nil {
+// 		t.Fatalf("Failed to register user : %s", err)
+// 	}
+
+// 	t.Logf("User ID : %s", userID)
+
+// 	if err := or.RegisterXPub(ctx, "m/0", xpubs, 1); err != nil {
+// 		t.Fatalf("Failed to register xpub : %s", err)
+// 	}
+
+// 	contractKey, err := bitcoin.GenerateKey(bitcoin.MainNet)
+// 	if err != nil {
+// 		t.Fatalf("Failed to generate contract key : %s", err)
+// 	}
+
+// 	contractAddress, err := contractKey.RawAddress()
+// 	contract := bitcoin.NewAddressFromRawAddress(contractAddress, bitcoin.MainNet).String()
+// 	assetCode := protocol.AssetCodeFromContract(contractAddress, 0)
+// 	asset := protocol.AssetID(assets.CodeCurrency, *assetCode)
+
+// 	receiver, blockHash, err := or.ApproveReceive(ctx, contract, asset, 1, 3, xpubs, 0, 1)
+// 	if err != nil {
+// 		t.Fatalf("Failed to approve receive : %s", err)
+// 	}
+
+// 	if err := or.ValidateReceiveHash(ctx, blockHash, contract, asset, receiver); err != nil {
+// 		t.Fatalf("Failed to validate receive : %s", err)
+// 	}
+// }
--- pkg/identity/receive.go
+++ pkg/identity/receive.go
@@ -19,26 +19,26 @@ var (
 
 // ApproveReceive requests a signature from the identity oracle to approve receipt of a token.
 func (o *Oracle) ApproveReceive(ctx context.Context, contract, asset string, oracleIndex int,
-	quantity uint64, xpub bitcoin.ExtendedKeys, index uint32, requiredSigners int) (*actions.AssetReceiverField, error) {
+	quantity uint64, xpubs bitcoin.ExtendedKeys, index uint32, requiredSigners int) (*actions.AssetReceiverField, bitcoin.Hash32, error) {
 
-	keys, err := xpub.ChildKeys(index)
+	keys, err := xpubs.ChildKeys(index)
 	if err != nil {
-		return nil, errors.Wrap(err, "generate key")
+		return nil, bitcoin.Hash32{}, errors.Wrap(err, "generate key")
 	}
 
 	address, err := keys.RawAddress(requiredSigners)
 	if err != nil {
-		return nil, errors.Wrap(err, "generate address")
+		return nil, bitcoin.Hash32{}, errors.Wrap(err, "generate address")
 	}
 
 	request := struct {
-		XPub     bitcoin.ExtendedKeys `json:"xpub"`
+		XPubs    bitcoin.ExtendedKeys `json:"xpubs"`
 		Index    uint32               `json:"index"`
 		Contract string               `json:"contract"`
 		AssetID  string               `json:"asset_id"`
 		Quantity uint64               `json:"quantity"`
 	}{
-		XPub:     xpub,
+		XPubs:    xpubs,
 		Index:    index,
 		Contract: contract,
 		AssetID:  asset,
@@ -51,15 +51,16 @@ func (o *Oracle) ApproveReceive(ctx context.Context, contract, asset string, ora
 			SigAlgorithm uint32            `json:"algorithm"`
 			Signature    bitcoin.Signature `json:"signature"`
 			BlockHeight  uint32            `json:"block_height"`
+			BlockHash    bitcoin.Hash32    `json:"block_hash"`
 		}
 	}
 
 	if err := post(o.BaseURL+"/transfer/approve", request, &response); err != nil {
-		return nil, errors.Wrap(err, "http post")
+		return nil, bitcoin.Hash32{}, errors.Wrap(err, "http post")
 	}
 
 	if !response.Data.Approved {
-		return nil, ErrNotApproved
+		return nil, bitcoin.Hash32{}, ErrNotApproved
 	}
 
 	result := &actions.AssetReceiverField{
@@ -71,7 +72,7 @@ func (o *Oracle) ApproveReceive(ctx context.Context, contract, asset string, ora
 		OracleSigBlockHeight:  response.Data.BlockHeight,
 	}
 
-	return result, nil
+	return result, response.Data.BlockHash, nil
 }
 
 // ValidateReceive checks the validity of an identity oracle signature for a receive.
@@ -106,6 +107,50 @@ func (o *Oracle) ValidateReceive(ctx context.Context, blocks BlockHashes, contra
 
 	sigHash, err := protocol.TransferOracleSigHash(ctx, contractRawAddress, assetCode.Bytes(),
 		receiveAddress, receiver.Quantity, blockHash, 1)
+	if err != nil {
+		return errors.Wrap(err, "signature hash")
+	}
+
+	logger.Info(ctx, "Validate receive signature hash : %x", sigHash)
+
+	signature, err := bitcoin.SignatureFromBytes(receiver.OracleConfirmationSig)
+	if err != nil {
+		return errors.Wrap(ErrInvalidSignature, "parse signature")
+	}
+
+	if !signature.Verify(sigHash, o.OracleKey) {
+		return errors.Wrap(ErrInvalidSignature, "validate signature")
+	}
+
+	return nil
+}
+
+// ValidateReceiveHash checks the validity of an identity oracle signature for a receive.
+func (o *Oracle) ValidateReceiveHash(ctx context.Context, blockHash bitcoin.Hash32,
+	contract, asset string, receiver *actions.AssetReceiverField) error {
+
+	if receiver.OracleSigAlgorithm != 1 {
+		return errors.New("Unsupported signature algorithm")
+	}
+
+	contractAddress, err := bitcoin.DecodeAddress(contract)
+	if err != nil {
+		return errors.Wrap(err, "decode contract address")
+	}
+	contractRawAddress := bitcoin.NewRawAddressFromAddress(contractAddress)
+
+	_, assetCode, err := protocol.DecodeAssetID(asset)
+	if err != nil {
+		return errors.Wrap(err, "decode asset id")
+	}
+
+	receiveAddress, err := bitcoin.DecodeRawAddress(receiver.Address)
+	if err != nil {
+		return errors.Wrap(err, "decode address")
+	}
+
+	sigHash, err := protocol.TransferOracleSigHash(ctx, contractRawAddress, assetCode.Bytes(),
+		receiveAddress, receiver.Quantity, &blockHash, 1)
 	if err != nil {
 		return errors.Wrap(err, "signature hash")
 	}
