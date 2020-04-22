4594f2c6eab4ddf65226133f4a94073306642bca
Curtis <curtis@nextcash.tech>
2020-04-21 22:06:48 -0600

    NEX-150 Add test for invalid master address. (#95)

--- cmd/smartcontractd/tests/contract_test.go
+++ cmd/smartcontractd/tests/contract_test.go
@@ -30,6 +30,7 @@ func TestContracts(t *testing.T) {
 	defer tests.Recover(t)
 
 	t.Run("create", createContract)
+	t.Run("masterAddress", masterAddress)
 	t.Run("oracle", oracleContract)
 	t.Run("amendment", contractAmendment)
 	t.Run("listAmendment", contractListAmendment)
@@ -156,6 +157,189 @@ func createContract(t *testing.T) {
 	// ********************************************************************************************
 	// Correct Contract Offer
 	offerData.BodyOfAgreementType = 2
+
+	// Reserialize and update tx
+	script, err = protocol.Serialize(&offerData, test.NodeConfig.IsTest)
+	if err != nil {
+		t.Fatalf("\t%s\tFailed to serialize offer : %v", tests.Failed, err)
+	}
+	offerTx.TxOut[1].PkScript = script
+
+	offerItx, err = inspector.NewTransactionFromWire(ctx, offerTx, test.NodeConfig.IsTest)
+	if err != nil {
+		t.Fatalf("\t%s\tFailed to create itx : %v", tests.Failed, err)
+	}
+
+	err = offerItx.Promote(ctx, test.RPCNode)
+	if err != nil {
+		t.Fatalf("\t%s\tFailed to promote itx : %v", tests.Failed, err)
+	}
+
+	err = offerItx.Validate(ctx)
+	if err != nil {
+		t.Fatalf("\t%s\tFailed to validate itx : %v", tests.Failed, err)
+	}
+
+	test.RPCNode.SaveTX(ctx, offerTx)
+
+	// Resubmit to handler
+	err = a.Trigger(ctx, "SEE", offerItx)
+	if err != nil {
+		t.Fatalf("\t%s\tFailed to handle contract offer : %v", tests.Failed, err)
+	}
+
+	t.Logf("Contract offer accepted")
+
+	// Check the response
+	checkResponse(t, "C2")
+
+	// Verify data
+	ct, err := contract.Retrieve(ctx, test.MasterDB, test.ContractKey.Address)
+	if err != nil {
+		t.Fatalf("\t%s\tFailed to retrieve contract : %v", tests.Failed, err)
+	}
+
+	if ct.ContractName != offerData.ContractName {
+		t.Fatalf("\t%s\tContract name incorrect : \"%s\" != \"%s\"", tests.Failed, ct.ContractName,
+			offerData.ContractName)
+	}
+
+	t.Logf("\t%s\tVerified contract name : %s", tests.Success, ct.ContractName)
+
+	if string(ct.BodyOfAgreement) != string(offerData.BodyOfAgreement) {
+		t.Fatalf("\t%s\tContract body incorrect : \"%s\" != \"%s\"", tests.Failed, string(ct.BodyOfAgreement), string(offerData.BodyOfAgreement))
+	}
+
+	t.Logf("\t%s\tVerified body name : %s", tests.Success, ct.BodyOfAgreement)
+
+	if ct.Issuer.Administration[0].Name != offerData.Issuer.Administration[0].Name {
+		t.Fatalf("\t%s\tContract issuer name incorrect : \"%s\" != \"%s\"", tests.Failed, ct.Issuer.Administration[0].Name, "John Smith")
+	}
+
+	t.Logf("\t%s\tVerified issuer name : %s", tests.Success, ct.Issuer.Administration[0].Name)
+
+	if !ct.HolderProposal {
+		t.Fatalf("\t%s\tContract holder proposal incorrect : %t", tests.Failed, ct.HolderProposal)
+	}
+
+	t.Logf("\t%s\tVerified holder proposal", tests.Success)
+
+	if ct.AdministrationProposal {
+		t.Fatalf("\t%s\tContract issuer proposal incorrect : %t", tests.Failed, ct.AdministrationProposal)
+	}
+
+	t.Logf("\t%s\tVerified issuer proposal", tests.Success)
+}
+
+func masterAddress(t *testing.T) {
+	ctx := test.Context
+
+	if err := resetTest(ctx); err != nil {
+		t.Fatalf("\t%s\tFailed to reset test : %v", tests.Failed, err)
+	}
+
+	addressText := "19ec6LTfLjbm9Ea1oJavAey4u6g1eNCzeu"
+	address, err := bitcoin.DecodeAddress(addressText)
+	if err != nil {
+		t.Fatalf("\t%s\tFailed to parse address : %v", tests.Failed, err)
+	}
+	ra := bitcoin.NewRawAddressFromAddress(address)
+
+	// New Contract Offer
+	offerData := actions.ContractOffer{
+		ContractName:        "Test Name",
+		BodyOfAgreementType: 2,
+		BodyOfAgreement:     []byte("This is a test contract and not to be used for any official purpose."),
+		Issuer: &actions.EntityField{
+			Type:           "I",
+			Administration: []*actions.AdministratorField{&actions.AdministratorField{Type: 1, Name: "John Smith"}},
+		},
+		VotingSystems:  []*actions.VotingSystemField{&actions.VotingSystemField{Name: "Relative 50", VoteType: "R", ThresholdPercentage: 50, HolderProposalFee: 50000}},
+		HolderProposal: true,
+		MasterAddress:  []byte(addressText),
+	}
+
+	// Create funding tx
+	fundingTx := tests.MockFundingTx(ctx, test.RPCNode, 1000014, issuerKey.Address)
+
+	// Build offer transaction
+	offerTx := wire.NewMsgTx(1)
+
+	offerInputHash := fundingTx.TxHash()
+
+	// From issuer (Note: empty sig script)
+	offerTx.TxIn = append(offerTx.TxIn, wire.NewTxIn(wire.NewOutPoint(offerInputHash, 0), make([]byte, 130)))
+
+	// To contract
+	script, _ := test.ContractKey.Address.LockingScript()
+	offerTx.TxOut = append(offerTx.TxOut, wire.NewTxOut(1000, script))
+
+	// Data output
+	script, err = protocol.Serialize(&offerData, test.NodeConfig.IsTest)
+	if err != nil {
+		t.Fatalf("\t%s\tFailed to serialize offer : %v", tests.Failed, err)
+	}
+	offerTx.TxOut = append(offerTx.TxOut, wire.NewTxOut(0, script))
+
+	offerItx, err := inspector.NewTransactionFromWire(ctx, offerTx, test.NodeConfig.IsTest)
+	if err != nil {
+		t.Fatalf("\t%s\tFailed to create itx : %v", tests.Failed, err)
+	}
+
+	err = offerItx.Promote(ctx, test.RPCNode)
+	if err != nil {
+		t.Fatalf("\t%s\tFailed to promote itx : %v", tests.Failed, err)
+	}
+
+	err = offerItx.Validate(ctx)
+	if err != nil {
+		t.Fatalf("\t%s\tFailed to validate itx : %v", tests.Failed, err)
+	}
+
+	test.RPCNode.SaveTX(ctx, offerTx)
+
+	err = a.Trigger(ctx, "SEE", offerItx)
+	if err == nil {
+		t.Fatalf("\t%s\tAccepted invalid contract offer", tests.Failed)
+	}
+	t.Logf("\t%s\tRejected invalid contract offer : %s", tests.Success, err)
+
+	// ********************************************************************************************
+	// Check reject response
+	if len(responses) != 1 {
+		t.Fatalf("\t%s\tHandle contract offer created no reject response", tests.Failed)
+	}
+
+	var responseMsg actions.Action
+	response := responses[0].Copy()
+	responses = nil
+	for _, output := range response.TxOut {
+		responseMsg, err = protocol.Deserialize(output.PkScript, test.NodeConfig.IsTest)
+		if err == nil {
+			break
+		}
+	}
+	if responseMsg == nil {
+		t.Fatalf("\t%s\tContract offer response doesn't contain tokenized op return", tests.Failed)
+	}
+	if responseMsg.Code() != "M2" {
+		t.Fatalf("\t%s\tContract offer response not a reject : %s", tests.Failed, responseMsg.Code())
+	}
+	reject, ok := responseMsg.(*actions.Rejection)
+	if !ok {
+		t.Fatalf("\t%s\tFailed to convert response to rejection", tests.Failed)
+	}
+	if reject.RejectionCode != actions.RejectionsMsgMalformed {
+		t.Fatalf("\t%s\tWrong reject code for contract offer reject : %d", tests.Failed,
+			reject.RejectionCode)
+	}
+
+	t.Logf("\t%s\tInvalid Contract offer rejection : (%d) %s", tests.Success, reject.RejectionCode,
+		reject.Message)
+
+	// ********************************************************************************************
+	// Correct Contract Offer
+	offerData.MasterAddress = ra.Bytes()
 
 	// Reserialize and update tx
 	script, err = protocol.Serialize(&offerData, test.NodeConfig.IsTest)
