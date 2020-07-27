9e20b582139c257f8f93626120857ecc1adf35b7
Curtis Ellis <curtis@nextcash.tech>
2020-07-26 19:00:00 -0600

    NEX-764 Implement entity contract tests.

--- cmd/smartcontractd/tests/contract_entity_test.go
+++ cmd/smartcontractd/tests/contract_entity_test.go
@@ -18,6 +18,7 @@ func TestContractEntities(t *testing.T) {
 	defer tests.Recover(t)
 
 	t.Run("createAssetContract", createAssetContract)
+	t.Run("createEntityContract", createEntityContract)
 	t.Run("identityContracts", identityContracts)
 }
 
@@ -123,6 +124,18 @@ func createAssetContract(t *testing.T) {
 			parent: nil,
 			valid:  false, // Entity contract formation won't exist
 		},
+		{
+			name: "AssetWithInvalidEntityAddress",
+			offer: &actions.ContractOffer{
+				ContractType: actions.ContractTypeAsset,
+				ContractName: "Test Name",
+				VotingSystems: []*actions.VotingSystemField{&actions.VotingSystemField{Name: "Relative 50",
+					VoteType: "R", ThresholdPercentage: 50, HolderProposalFee: 50000}},
+				EntityContract: []byte{0x20, 0x30, 0xf8},
+			},
+			parent: nil,
+			valid:  false, // Entity contract address is not valid
+		},
 	}
 
 	for _, tt := range testSet {
@@ -219,6 +232,168 @@ func createAssetContract(t *testing.T) {
 	}
 }
 
+func createEntityContract(t *testing.T) {
+	ctx := test.Context
+
+	testSet := []struct {
+		name  string
+		offer *actions.ContractOffer
+		valid bool
+	}{
+		{
+			name: "ValidEntity",
+			offer: &actions.ContractOffer{
+				ContractType: actions.ContractTypeEntity,
+				ContractName: "Test Name",
+				Issuer: &actions.EntityField{
+					Name: "John Bitcoin",
+				},
+				VotingSystems: []*actions.VotingSystemField{&actions.VotingSystemField{Name: "Relative 50",
+					VoteType: "R", ThresholdPercentage: 50, HolderProposalFee: 50000}},
+			},
+			valid: true,
+		},
+		{
+			name: "EntityWithParent",
+			offer: &actions.ContractOffer{
+				ContractType: actions.ContractTypeEntity,
+				ContractName: "Test Name",
+				Issuer: &actions.EntityField{
+					Name: "John Bitcoin",
+				},
+				VotingSystems: []*actions.VotingSystemField{&actions.VotingSystemField{Name: "Relative 50",
+					VoteType: "R", ThresholdPercentage: 50, HolderProposalFee: 50000}},
+				EntityContract: test.Contract2Key.Address.Bytes(),
+			},
+			valid: false,
+		},
+		{
+			name: "EntityWithOutIssuer",
+			offer: &actions.ContractOffer{
+				ContractType: actions.ContractTypeEntity,
+				ContractName: "Test Name",
+				VotingSystems: []*actions.VotingSystemField{&actions.VotingSystemField{Name: "Relative 50",
+					VoteType: "R", ThresholdPercentage: 50, HolderProposalFee: 50000}},
+			},
+			valid: true,
+		},
+		{
+			name: "EntityWithService",
+			offer: &actions.ContractOffer{
+				ContractType: actions.ContractTypeEntity,
+				ContractName: "Test Name",
+				VotingSystems: []*actions.VotingSystemField{&actions.VotingSystemField{Name: "Relative 50",
+					VoteType: "R", ThresholdPercentage: 50, HolderProposalFee: 50000}},
+				Services: []*actions.ServiceField{
+					&actions.ServiceField{
+						Type:      actions.OracleTypeIdentity,
+						PublicKey: oracleKey.Key.PublicKey().Bytes(),
+					},
+				},
+			},
+			valid: true,
+		},
+		{
+			name: "EntityWithInvalidService",
+			offer: &actions.ContractOffer{
+				ContractType: actions.ContractTypeEntity,
+				ContractName: "Test Name",
+				VotingSystems: []*actions.VotingSystemField{&actions.VotingSystemField{Name: "Relative 50",
+					VoteType: "R", ThresholdPercentage: 50, HolderProposalFee: 50000}},
+				Services: []*actions.ServiceField{
+					&actions.ServiceField{
+						Type:      actions.OracleTypeIdentity,
+						PublicKey: []byte{0x03, 0x00, 0xff, 0x1d},
+					},
+				},
+			},
+			valid: false,
+		},
+	}
+
+	for _, tt := range testSet {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := resetTest(ctx); err != nil {
+				t.Fatalf("\t%s\tFailed to reset test : %v", tests.Failed, err)
+			}
+
+			// Create funding tx
+			fundingTx := tests.MockFundingTx(ctx, test.RPCNode, 100104, issuerKey.Address)
+
+			// Build offer transaction
+			offerTx := wire.NewMsgTx(1)
+
+			offerInputHash := fundingTx.TxHash()
+
+			// From issuer (Note: empty sig script)
+			offerTx.TxIn = append(offerTx.TxIn, wire.NewTxIn(wire.NewOutPoint(offerInputHash, 0),
+				make([]byte, 130)))
+
+			// To contract
+			script, _ := test.ContractKey.Address.LockingScript()
+			offerTx.TxOut = append(offerTx.TxOut, wire.NewTxOut(1000, script))
+
+			// Data output
+			var err error
+			script, err = protocol.Serialize(tt.offer, test.NodeConfig.IsTest)
+			if err != nil {
+				t.Fatalf("\t%s\tFailed to serialize offer : %v", tests.Failed, err)
+			}
+			offerTx.TxOut = append(offerTx.TxOut, wire.NewTxOut(0, script))
+
+			offerItx, err := inspector.NewTransactionFromWire(ctx, offerTx, test.NodeConfig.IsTest)
+			if err != nil {
+				t.Fatalf("\t%s\tFailed to create itx : %v", tests.Failed, err)
+			}
+
+			err = offerItx.Promote(ctx, test.RPCNode)
+			if err != nil {
+				t.Fatalf("\t%s\tFailed to promote itx : %v", tests.Failed, err)
+			}
+
+			err = offerItx.Validate(ctx)
+			if err != nil {
+				t.Fatalf("\t%s\tFailed to validate itx : %v", tests.Failed, err)
+			}
+
+			test.RPCNode.SaveTX(ctx, offerTx)
+
+			err = a.Trigger(ctx, "SEE", offerItx)
+			if tt.valid {
+				if err != nil {
+					t.Fatalf("\t%s\tFailed to process contract offer : %s", tests.Failed, err)
+				}
+
+				// Check the response
+				checkResponse(t, "C2")
+
+				t.Logf("\t%s\tContract offer accepted", tests.Success)
+
+				// Verify data
+				cf, err := contract.FetchContractFormation(ctx, test.MasterDB, test.ContractKey.Address,
+					test.NodeConfig.IsTest)
+				if err != nil {
+					t.Fatalf("\t%s\tFailed to retrieve contract : %v", tests.Failed, err)
+				}
+
+				if !tt.offer.Issuer.Equal(cf.Issuer) {
+					t.Fatalf("\t%s\tWrong contract issuer : %+v != %+v", tests.Failed, cf.Issuer,
+						tt.offer.Issuer)
+				}
+			} else {
+				if err == nil {
+					t.Errorf("\t%s\tFailed to reject contract offer", tests.Failed)
+				}
+
+				// Check the response
+				checkResponse(t, "M2")
+
+				t.Logf("\t%s\tContract offer rejected", tests.Success)
+			}
+		})
+	}
+}
+
 func identityContracts(t *testing.T) {
 	ctx := test.Context
 
@@ -262,7 +437,7 @@ func identityContracts(t *testing.T) {
 			valid:         true,
 		},
 		{
-			name: "ValidAssetWithIdentity",
+			name: "ValidAssetWithMissingIdentity",
 			offer: &actions.ContractOffer{
 				ContractType: actions.ContractTypeAsset,
 				ContractName: "Test Name",
@@ -280,7 +455,7 @@ func identityContracts(t *testing.T) {
 			valid:         false, // identity oracle contract won't exist
 		},
 		{
-			name: "ValidAssetWithIdentity",
+			name: "ValidAssetWithWrongIdentity",
 			offer: &actions.ContractOffer{
 				ContractType: actions.ContractTypeAsset,
 				ContractName: "Test Name",
